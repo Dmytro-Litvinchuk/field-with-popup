2bcd141702d3983d6c17f7e64045984f585ddc0c
Dmytro-Litvinchuk <dimawaw11@gmail.com>
2020-02-10 13:47:08 +0200

    beta

--- js/custom.js
+++ js/custom.js
@@ -1,12 +1,19 @@
 (function ($, Drupal) {
   Drupal.behaviors.jsPopupSimple = {
     attach: function (context, settings) {
-      $("body").click(function (){
-        $('.pop-up').once('popped').each(function (i, popupElement) {
-          var myPopup = Drupal.dialog(popupElement, {width: 'auto'});
-          myPopup.showModal();
+      // Check exist class.
+      if ($(".pop-up")[0]){
+        $(".field--name-field-url-video").hide();
+        $(".field--name-field-img img").css("cursor", "pointer");
+        $(".field--name-field-img").click(function (){
+          $(".pop-up").once("popped").each(function (i, popupElement) {
+            var myPopup = Drupal.dialog(popupElement, {width: "auto"});
+            myPopup.showModal();
+          });
         });
-      });
+      } else if ($(".default-video")[0]) {
+        $(".field--name-field-img").hide();
+      }
     }
   };
 })(jQuery, Drupal);
